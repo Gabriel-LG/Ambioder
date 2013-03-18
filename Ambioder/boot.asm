32338f687f297106441ea2589740580fa7352a5c
Gabriel-LG <l.gorter@gmail.com>
2013-03-18 23:31:54 +0100

    Fixed issue that required 2 stop bits

--- Ambioder/boot.asm
+++ Ambioder/boot.asm
@@ -77,8 +77,8 @@ initialize
     bcf PIR1, TMR2IF
     bsf STATUS, RP0 ; bank1
     bsf PIE1, TMR2IE; enable timer1 interrupt
-    ;movlw d'69' ; 9600hz
-    movlw d'35' ; 19200hz
+    ;movlw d'68' ; 9600hz
+    movlw d'34' ; 19200hz
     movwf PR2
     bcf STATUS, RP0 ; bank0
     movlw b'00000100' ; Finstr, 1:1, on
